5d1cb109b4c32da6fb407e50c8efebeaf2688460
Jaywant Manurkar <manurkarjaywant@gmail.com>
2025-01-20 19:41:10 +0530

    theme switcher completed

--- 09themeswitcher/tailwind.config.js
+++ 09themeswitcher/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",    // this line is important when switching between dark and light themes.  is is not added in the config file by default.
   theme: {
     extend: {},
   },
